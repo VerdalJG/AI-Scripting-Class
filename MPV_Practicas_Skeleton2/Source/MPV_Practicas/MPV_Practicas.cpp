8e45bd1f0b8704026d1ce8ab16cf1120084cc216
Verdal <verdaljg@gmail.com>
2025-01-15 13:20:06 +0100

    Removed unnecessary files

--- MPV_Practicas_Skeleton2/Source/MPV_Practicas/MPV_Practicas.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "MPV_Practicas.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MPV_Practicas, "MPV_Practicas" );
--- MPV_Practicas_Skeleton2/Source/MPV_Practicas/util.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-inline float convertTo360(float a);
-
-float convertTo180(float a);
-
-float sign(float a);
